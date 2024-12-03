118c2d6021731518fbb78f4c4e4ddc11d671702d
Clemens Näther <naether.clemens@gmail.com>
2024-12-03 06:47:29 +0100

    2024-03

--- 2024/day3/Day3.java
+++ 2024/day3/Day3.java
@@ -2,7 +2,6 @@ package day3;
 import Template.Template;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Day3 {
@@ -21,18 +20,71 @@ public class Day3 {
             
             // part 1
             int sum = 0;
+            String regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)";
+
             for (String line : inputLines) {
+                for (int i = 0; i < line.length()-8; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        if (line.length() < i+8+j) break;
+                        String sub = line.substring(i, i+8+j);
+                        if (sub.matches(regex)) {
+                            sum += multiply(sub);
+                            continue;
+                        }
+                    }
+                }
             }
+            System.out.println(sum);
+
 
             // part 2
             sum = 0;
+            boolean enabled = true;
+
             for (String line : inputLines) {
+                for (int i = 0; i < line.length()-8; i++) {
+                    String dont = line.substring(i,i+7);
+                    String doo = line.substring(i,i+4);
+                    if (dont.equals("don't()")) {
+                        enabled = false;
+                    }
+                    if (doo.equals("do()")) {
+                        enabled = true;
+                    }
+
+                    if (!enabled) continue;
+
+                    for (int j = 0; j < 5; j++) {
+                        if (line.length() < i+8+j) break;
+                        String sub = line.substring(i, i+8+j);
+                        if (sub.matches(regex)) {
+                            sum += multiply(sub);
+                            continue;
+                        }
+                    }
+                }
             }
-            
+            System.out.println(sum);
+
+
+
+            // part 2
 
         } catch (IOException e) {
             System.out.println("Error reading input file: " + e.getMessage());
             e.printStackTrace();
         }
+
+        
+    }
+    public static int multiply(String sub) {
+        String num1Str = sub.substring(4, sub.indexOf(","));
+        String num2Str = sub.substring(sub.indexOf(",")+1, sub.length()-1);
+        int num1 = Integer.parseInt(num1Str);
+        int num2 = Integer.parseInt(num2Str);
+        // System.out.println(sub);
+        // System.out.println("num1: " + num1 + " num2: " + num2);
+        // System.out.println();
+        return num1 * num2;
     }
 }
